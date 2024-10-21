83a6155b867cbdde7bfb595e9e681b7d36d30d6a
mloper23 <mloper23@eafit.edu.co>
2024-10-21 16:03:32 +0200

    P and Q variables included for better following up on sample and sensor sizes.
    Function resize updated to account for imaginary and real part separately instead of amplitude and phase.

--- dlhm.py
+++ dlhm.py
@@ -34,21 +34,28 @@ def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
     # Determine the size of the input sample
     N, M = sample.shape
 
+    # The discretization on the camera is set as the same input's discretization
+    P = M
+    Q = N
+
     # Magnification factor
     Mag = L / z
-    W_s = W_c / Mag
+    W_s = W_c / Mag  # the size of the window (field of view) that will be propagated
 
     # Re-sample if desired
     if dx_in != 0:
-        res_d = dx_in * Mag / dx_out
+        res_d = dx_in * Mag / dx_out  # ratio of resizing considering the input sample's pixel and the desired
+        # magnification
         sample = resize(sample, int(M / res_d), int(N / res_d))
         N_s, M_s = sample.shape
-        W_sample = N_s * dx_out / Mag
+        W_sample = N_s * dx_out / Mag  # the size of the provided field of view is computed
 
-        if W_sample > W_s:
+        if W_sample > W_s:  # if the field of view provided is larger that the field of view that the system captures,
+            # then only the illuminated portion is taken. Else, it is assumed that the provided field
+            # of view is the illuminated portion.
             sample = sample[N_s // 2 - N // 2:N_s // 2 + N // 2, M_s // 2 - M // 2:M_s // 2 + M // 2]
         else:
-            sample = resize(sample, M, N)
+            sample = resize(sample, P, Q)
 
         # Crop the sample based on the magnification and offsets
         start_x = int(N / 2 - N / (Mag * 2) + x0)
@@ -58,18 +65,14 @@ def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
         sample = sample[start_x:end_x, start_y:end_y]
 
         # Resample to match sensor characteristics
-        # rs = sample.shape[0] / (W_c / dx_out)
-        sample = resize(sample, M, N)
-
-    # Ensure the updated sample size
-    N, M = sample.shape
+        sample = resize(sample, P, Q)
 
     # Wave number
     k = 2 * np.pi / wavelength
 
     # Spatial coordinates in the camera's plane
-    x = np.linspace(-W_c / 2, W_c / 2, M)
-    y = np.linspace(-W_c / 2, W_c / 2, N)
+    x = np.linspace(-W_c / 2, W_c / 2, P)
+    y = np.linspace(-W_c / 2, W_c / 2, Q)
     u, v = np.meshgrid(x, y)
 
     # Radial distances for source wavefront
@@ -98,11 +101,11 @@ def dlhm(sample, dx_in, L, z, W_c, dx_out, wavelength, x0=0, y0=0, NA_s=0):
 
     # Distortion of the coordinates
     # Calculate maximum distortion due to distance differences
-    Max_D = np.abs((L + np.abs(np.sqrt(W_c**2 / 2 + L**2) - L)) / z - L / z)
+    Mag_max = np.sqrt(W_c ** 2 / 2 + L ** 2) / z
 
     # Apply distortion to the hologram
-    camMat = np.array([[N, 0, N / 2], [0, M, M / 2], [0, 0, 1]])
-    distCoeffs = np.array([-Max_D / (N/2), 0, 0, 0, 0])  # Radial distortion parameters
+    camMat = np.array([[P, 0, P / 2], [0, Q, Q / 2], [0, 0, 1]])
+    distCoeffs = np.array([-Mag_max / (2 * Mag), 0, 0, 0, 0])  # Radial distortion parameters
     holo = cv.undistort(holo.astype(np.float32), camMat, distCoeffs)
 
     # Normalize and post-process the hologram
@@ -122,11 +125,12 @@ def ifts(A):
 def fts(A):
     return np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(A)))
 
+
 # Customized function for complex resize
 def resize(B, dx, dy):
-    am = np.abs(B)
-    ph = np.angle(B)
-    am_r = cv.resize(am, (int(dx), int(dy)), interpolation=cv.INTER_LINEAR)
-    ph_r = cv.resize(ph, (int(dx), int(dy)), interpolation=cv.INTER_LINEAR)
-    B_r = am_r * np.exp(1j * ph_r)
+    r = np.real(B)
+    img = np.imag(B)
+    r_r = cv.resize(r, (int(dx), int(dy)), interpolation=cv.INTER_LINEAR)
+    img_r = cv.resize(img, (int(dx), int(dy)), interpolation=cv.INTER_LINEAR)
+    B_r = r_r + 1j * img_r
     return B_r
--- main_dlhm.py
+++ main_dlhm.py
@@ -12,14 +12,14 @@ sample = np.exp(-1j * 2 * np.pi * (1.51 - 1) * h_max * intensityImage / lambda_)
 # sample = 1-intensityImage
 
 # Simulation parameters
-L = 4e-3  # Distance from the source to the hologram plane
-z = 1e-3  # Distance from the source to the sample's plane
+L = 8e-3  # Distance from the source to the hologram plane
+z = 7.8e-3  # Distance from the source to the sample's plane
 W_c = 5.55e-3  # Width of the sensor
 lambda_ = 532e-9  # Wavelength of the light
 dx_in = 1.85e-6
 
 # Call the realistic_dlhm function to simulate digital lensless holograms
-holo = dlhm(sample, dx_in, L, z, W_c, dx_in, lambda_, x0=0, y0=0, NA_s=0.1)
+holo = dlhm(sample, dx_in, L, z, W_c, dx_in, lambda_, x0=0, y0=0, NA_s=0.5)
 
 # Display the simulated hologram
 fig = px.imshow(holo, color_continuous_scale='gray')
